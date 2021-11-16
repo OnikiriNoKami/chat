32741c9f6b8e430a8113db36c217773a9f35b5b9
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-16 15:54:01 +0200

    basic controlles for socket

--- backend/src/socket/index.js
+++ backend/src/socket/index.js
@@ -1,10 +1,11 @@
 import { Server } from "socket.io";
+import socketController from "./controllers/socketController";
 
 const ioServer = (app) => {
     const io = new Server(app);
 
     console.log('Socket io initialized.')
-
+    io.on('connection', socket => socketController(socket));
     return io;
 }
 
