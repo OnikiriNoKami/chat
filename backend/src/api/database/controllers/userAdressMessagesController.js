bae4a925253231f949aa69f74c09d866ece0d626
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-17 16:23:10 +0200

    use address messages controller

---
+++ backend/src/api/database/controllers/userAdressMessagesController.js
@@ -0,0 +1,18 @@
+import UserAddressMessages from "../schemas/userAddressMessagesSchema";
+import mongoose from 'mongoose';
+
+const userAddressMessagesController = {
+    create: async ({user, address}) => {
+        if(!user || !address) return new Error("No data provided.")
+        try{
+            const userAddress = new UserAddressMessages({user, address});
+            await userAddress.save();
+            return userAddress;
+        } catch (error){
+            console.log(error.message);
+            return error
+        }
+    }
+}
+
+export default userAddressMessagesController;
