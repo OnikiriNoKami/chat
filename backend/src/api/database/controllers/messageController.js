f5d183712f664efa8b6685e29e2bf3cac41d2206
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-17 15:25:26 +0200

    message deletion and retriving by sender,address with pagination

--- backend/src/api/database/controllers/messageController.js
+++ backend/src/api/database/controllers/messageController.js
@@ -1,18 +1,50 @@
-import Message from '../schemas/messageSchema';
+import Message from "../schemas/messageSchema";
+import mongoose from "mongoose";
 
 const messageController = {
-    create: async ({sender, address, body}) => {
-        console.log(" in controller")
-        if(!sender || !address || !body) return new Error("Something wrong with pdovided data.")
-        try{
-            const message = new Message({sender, address, body});
+    create: async ({ sender, address, body }) => {
+        if (!sender || !address || !body)
+            return new Error("Something wrong with provided data.");
+        try {
+            const message = new Message({ sender, address, body });
             await message.save();
             return message;
-        } catch (error){
+        } catch (error) {
             console.log(error.message);
             return error;
         }
-    }
-}
+    },
+    delete: async (messageId) => {
+        if (!messageId) return new Error("No data provided.");
+        try {
+            const result = await Message.findByIdAndDelete(messageId);
+            return result;
+        } catch (error) {
+            console.log(error.message);
+            return error;
+        }
+    },
+    getBySenderAndAddress: async ({
+        sender,
+        address,
+        limit = 25,
+        lastMessageId,
+    }) => {
+        if (!sender) return new Error("No sender provided.");
+        try {
+            const result = await Message.find({
+                _id: { $lt: mongoose.Types.ObjectId(lastMessageId) },
+                sender: sender,
+                address: address,
+            })
+                .sort({ createdAt: "desc" })
+                .limit(limit);
+            return result;
+        } catch (error) {
+            console.log(error.message);
+            return error;
+        }
+    },
+};
 
 export default messageController;
