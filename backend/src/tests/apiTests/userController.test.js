ca9febb12f7efa42a624b3de899ee514f0dec5da
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-18 14:22:33 +0200

    user find by email, displayName and with no data tests

--- backend/src/tests/apiTests/userController.test.js
+++ backend/src/tests/apiTests/userController.test.js
@@ -15,21 +15,38 @@ describe('User address messages controller tests.', () => {
         email = "testUser@email.com";
     });
 
-    test("Creting user.", async()=>{
-        const result = await userController.create({displayName, email});
+    // test("Creting user.", async()=>{
+    //     const result = await userController.create({displayName, email});
+    //     expect(result instanceof Error).toBe(false);
+    //     expect(result.displayName).toBe(displayName);
+    //     expect(result.email).toBe(email);
+    //     userId = result._id;
+    // })
+
+    // test('Deleting user.', async()=>{
+    //     const result = await userController.delete(userId);
+    //     expect(result instanceof Error).toBe(false);
+    //     expect(result.displayName).toBe(displayName);
+    //     expect(result.email).toBe(email);
+    // })
+    test('Find user by email.', async()=>{
+        const result = await userController.findUser({ email });
         expect(result instanceof Error).toBe(false);
         expect(result.displayName).toBe(displayName);
         expect(result.email).toBe(email);
-        userId = result._id;
     })
 
-    test('Deleting user.', async()=>{
-        const result = await userController.delete(userId);
+    test('Find user by displayNaame.', async()=>{
+        const result = await userController.findUser({ displayName });
         expect(result instanceof Error).toBe(false);
         expect(result.displayName).toBe(displayName);
         expect(result.email).toBe(email);
     })
 
+    test('Find user no data provided.', async()=>{
+        const result = await userController.findUser();
+        expect(result instanceof Error).toBe(true);
+    })
 
     afterAll(async () => {
         await mongoose.connection.close()
