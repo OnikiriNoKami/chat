d430b7842de0211715cc9c100cd120bd31bb38a8
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-18 16:09:11 +0200

    next call fix

--- backend/src/middlewares/socketMiddlewares/credentialsMidleware/userDataCredentials.js
+++ backend/src/middlewares/socketMiddlewares/credentialsMidleware/userDataCredentials.js
@@ -5,11 +5,11 @@ const userDataCredentials = async (socket, next) => {
     if(!displayName || !email) return next(new Error('No user data provided.'));
 
     const result = await userController.findUser({displayName, email});
-
     if(!result) {
         const newUser = await userController.create({displayName, email})
         if(newUser instanceof Error) return next(newUser);
         socket.handshake.auth._id = newUser._id;
+        next();
     };
     socket.handshake.auth._id = result._id;
     next();
