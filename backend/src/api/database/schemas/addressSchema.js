d8375318dbcaf03cbcd3d425bb136b825fc8ec78
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-18 13:54:00 +0200

    address & user schema

---
+++ backend/src/api/database/schemas/addressSchema.js
@@ -0,0 +1,10 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const addressSchema = new mongoose.Schema(
+    {
+        address: { type: String, required: true },
+    },
+    { timestamps: true }
+);
+
+export default addressSchema;
