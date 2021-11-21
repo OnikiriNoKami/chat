6362912a6f87c09f0c0701b5b08780e220f3e12b
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-21 18:24:18 +0200

    user controller code refactoring

--- backend/src/api/database/controllers/userController.js
+++ backend/src/api/database/controllers/userController.js
@@ -3,37 +3,22 @@ import User from "../schemas/userSchema";
 const userController = {
     create: async ({ displayName, email }={}) => {
         if (!displayName || !email) return new Error("No data provided");
-        try {
-            const user = new User({ displayName, email });
+        const user = new User({ displayName, email });
             await user.save();
 
             return user;
-        } catch (error) {
-            console.log(error.message);
-            return error;
-        }
     },
     delete: async (_id) => {
         if (!_id) return new Error("No data provided");
-        try {
-            const result = await User.findByIdAndDelete({_id});
+        const result = await User.findByIdAndDelete({_id});
             return result;
-        } catch (error) {
-            console.log(error.message);
-            return error;
-        }
     },
     findUser: async ({ email, displayName }={}) => {
         if (!displayName && !email) return new Error("No data provided");
-        try {
-            const result = await User.findOne({
-                $or: [{ email }, { displayName }],
-            });
-            return result;
-        } catch (error) {
-            console.log(error.message);
-            return error;
-        }
+        const result = await User.findOne({
+            $or: [{ email }, { displayName }],
+        });
+        return result;
     },
 };
 
