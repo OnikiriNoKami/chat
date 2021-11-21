a58338da39624b0a3bd027709c9daa2fd784bc98
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-21 18:23:51 +0200

    socket events types as object props

---
+++ backend/src/socket/utils/socketEventTypes.js
@@ -0,0 +1,8 @@
+ const socketEventTypes = {
+    findUser: 'findUser',
+    giveMyId: 'giveMyId',
+    hello: 'hello',
+    connect: 'connect',
+    connectError: 'connect_error',
+}
+export default socketEventTypes;
